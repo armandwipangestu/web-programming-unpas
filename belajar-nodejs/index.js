077e41f1c8843a028c6a04db2b3aa7109a393ea1
armandwipangestu <armandwi.pangestu7@gmail.com>
2022-06-04 22:48:11 +0700

    NodeJS run app

---
+++ belajar-nodejs/index.js
@@ -0,0 +1,8 @@
+// const fs = require('fs') // core module
+// const cetakNama = require('./coba') // local module
+// const moment = require('moment') // third party module / npm module / node_modules
+
+// const cetakNama = require('./coba')
+const coba = require('./coba')
+
+console.log(coba.cetakNama('Arman Dwi Pangestu'), coba.PI, coba.mahasiswa.cetakMhs(), new coba.Orang)
